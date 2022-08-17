1df2ee03506204ef0af3f76ca8bc9bea8588f637
lazar-r <42014334+lazar-r@users.noreply.github.com>
2022-08-17 17:50:48 +0200

    second commit

--- JS/forecast.js
+++ JS/forecast.js
@@ -2,7 +2,7 @@ const key = '5D0Fs8fgUzIztB7HaELZhXTp79fgh44Y';
 
 //get weather info
 const getWeather = async (id) => {
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
@@ -13,7 +13,7 @@ const getWeather = async (id) => {
 
 // get city info
 const getCity = async (city) => {
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
